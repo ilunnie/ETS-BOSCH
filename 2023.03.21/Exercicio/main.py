902197b289572d5e7489c910aca416da6b7a1dda
Luis Gustavo <67930760+ilunnie@users.noreply.github.com>
2023-03-30 16:45:55 -0300

    Adicionado 2023.03.21
    Adicionando as atividades do dia 21/03/2023

---
+++ 2023.03.21/Exercicio/main.py
@@ -0,0 +1,37 @@
+from Luis import *
+from LuizMod import *
+from Vini import *
+
+def recebe_int(texto, erro="Valor inválido!"):
+    while True:
+        try:
+            return float(input(texto))
+        except:
+            print(erro)
+
+while True:
+    print('\n'*30)
+    num1 = recebe_int("\nDigite o primeiro número\n> ")
+    num2 = recebe_int("\nDigite o segundo número\n> ")
+
+    operacao = input("\nQual operação deseja fazer?\n> ").replace('*', 'x')
+
+    match operacao:
+        case "+":
+            print(f"Resultado: {soma(num1, num2)}")
+            break
+        case "-":
+            print(f"Resultado: {subtracao(num1, num2)}")
+            break
+        case "x":
+            print(f"Resultado: {x(num1, num2)}")
+            break
+        case "/":
+            print(f"Resultado: {div(num1, num2)}")
+            break
+        case "fs":
+            print(f'Resultado: {fatorialsoma(num1, num2)}')
+            break
+        case other:
+            print('\nEssa operação não existe.')
+            break
